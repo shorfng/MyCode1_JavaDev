6d90fd752c3f151269bdaba069911cede70ff545
蓝田_Loto <shorfng@126.com>
2022-01-15 23:06:27 +0800

    更新：getClass（）和 .class 验证代码

--- Java_1.2_Oriented/src/main/java/com/loto/oriented/g/polymorphism/f/instanceofsymbol/Main.java
+++ Java_1.2_Oriented/src/main/java/com/loto/oriented/g/polymorphism/f/instanceofsymbol/Main.java
@@ -12,5 +12,16 @@ public class Main {
         // 创建一个猫对象
         Cat cat = new Cat();
         p.function(cat); // 猫捉老鼠
+
+        // ===============================================
+        // 得到 对象cat 的类型
+        System.out.println(cat.getClass()); // class com.loto.oriented.g.polymorphism.f.instanceofsymbol.Cat
+
+        // 得到 类Cat 的类型
+        System.out.println(Cat.class);      // class com.loto.oriented.g.polymorphism.f.instanceofsymbol.Cat
+
+        // 比较 对象cat的类型 和 类Cat的类型 是否一致
+        // 返回的是一个 boolean 类型的值
+        System.out.println(cat.getClass() == Cat.class);  // true
     }
 }
